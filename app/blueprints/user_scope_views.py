cd90ee19fc63f04ab7ad355c48456fce031a587b
Gabriel Andres Anzola Tachak <gabriel.anzola@mercadolibre.com.co>
2022-09-10 00:52:37 -0500

    get users scopes

--- app/blueprints/user_scope_views.py
+++ app/blueprints/user_scope_views.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 
 from flask import Blueprint, jsonify
 
+import app
 from app.decorators import error_decorator
 from app.utils.constants import PING_RESPONSE
 
@@ -14,3 +15,11 @@ user_scope_bp = Blueprint("user_scope", __name__)
 def ping_pong():
     """Ping endpoint, used to know if the app is up."""
     return jsonify(PING_RESPONSE), HTTPStatus.OK
+
+
+@user_scope_bp.route("/<string:user_id>", methods=["GET"])
+@error_decorator
+def get_user_scopes(user_id: str):
+    """Returns every role assigned to a user"""
+    roles = app.user_scope_controller.get_user_scopes(user_id)
+    return jsonify({"scopes": roles}), HTTPStatus.OK
--- app/controllers/user_scope_controller.py
+++ app/controllers/user_scope_controller.py
@@ -1,6 +1,24 @@
 from sqlalchemy.orm import session
 
+from app.entities.role_scope_entity import RoleScopeEntity
+from app.entities.scope_entity import ScopeEntity
+from app.entities.user_role_entity import UserRoleEntity
+from app.log import logger
+
 
 class UserScopeController:
     def __init__(self, db_session: session = None):
         self.db_session = db_session
+
+    def get_user_scopes(self, user_id: str):
+        """Get scopes assigned to a specified user"""
+
+        user_roles = self.db_session.query(UserRoleEntity).filter_by(user_id=user_id).all()
+        roles_id = [user_role.to_dict().get("role_id") for user_role in user_roles]
+
+        user_scopes = self.db_session.query(RoleScopeEntity).filter(RoleScopeEntity.role_id.in_(roles_id)).all()
+        scopes_id = [user_scope.to_dict().get("scope_id") for user_scope in user_scopes]
+
+        scopes = self.db_session.query(ScopeEntity).filter(ScopeEntity.scope_id.in_(scopes_id)).all()
+
+        return [scope.to_dict().get("name") for scope in scopes]
