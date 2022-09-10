1ef8d55dc66caa77cec574dd0a6452360588a0d2
Gabriel Andres Anzola Tachak <gabriel.anzola@mercadolibre.com.co>
2022-09-09 22:52:14 -0500

    Mini fix to logger and roles

--- app/controllers/user_role_controller.py
+++ app/controllers/user_role_controller.py
@@ -12,9 +12,9 @@ class UserRoleController:
     def get_user_roles(self, user_id: str):
         """Get roles assigned to a specified user"""
 
-        user_roles = self.db_session.query(UserRoleEntity).filter_by(user_id=user_id)
+        user_roles = self.db_session.query(UserRoleEntity).filter_by(user_id=user_id).all()
 
-        roles_id = [user_role.to_dict().get("role_id") for user_role in user_roles.all()]
-        roles = self.db_session.query(RoleEntity).filter(RoleEntity.role_id.in_(roles_id))
+        roles_id = [user_role.to_dict().get("role_id") for user_role in user_roles]
+        roles = self.db_session.query(RoleEntity).filter(RoleEntity.role_id.in_(roles_id)).all()
 
-        return [role.to_dict().get("name") for role in roles.all()]
+        return [role.to_dict().get("name") for role in roles]
--- app/log.py
+++ app/log.py
@@ -1,20 +1,17 @@
 import logging
 from logging.config import dictConfig
 
-dictConfig({
+FORMAT = '%(asctime)s - [request_id:%(request_id)s] - %(module)s.%(funcName)s:%(lineno)d - %(levelname)s: %(message)s'
+
+DICT_CONFIG = {
     'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
+    'formatters': {
+        'default': {
+            'format': FORMAT
+        }
+    },
+}
+
+dictConfig(DICT_CONFIG)
 
 logger = logging.getLogger(__name__)
