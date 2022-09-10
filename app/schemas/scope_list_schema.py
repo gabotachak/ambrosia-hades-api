ac636136b6c71ecef7a11e667a51f8402ac9a858
Gabriel Andres Anzola Tachak <gabriel.anzola@mercadolibre.com.co>
2022-09-10 13:20:11 -0500

    Length validation scopelist

--- app/schemas/scope_list_schema.py
+++ app/schemas/scope_list_schema.py
@@ -1,5 +1,5 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class ScopeListSchema(Schema):
-    scope_list = fields.List(fields.String())
+    scope_list = fields.List(fields.String(required=True, validate=validate.Length(min=4, max=20)))
