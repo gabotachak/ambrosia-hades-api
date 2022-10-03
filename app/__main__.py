1bde69100b79942f69b04498bf32be6aef94d131
pmoros <pmoros@outlook.com>
2022-10-02 19:41:26 -0500

    fix: port failure in Docker

--- app/__main__.py
+++ app/__main__.py
@@ -5,4 +5,4 @@ from app import create_app
 app = create_app(start_orm=True)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=4000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
